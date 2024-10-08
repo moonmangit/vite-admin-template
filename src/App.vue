c67bf088e8c8570808d5e06104f007ae7e97020a
Moonmangit <81956440+moonmangit@users.noreply.github.com>
2024-10-08 10:33:21 +0700

    add : lang switcher

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <template>
-  <div>
-    <nav>
-      <RouterLink
-        v-for="(route, idx) in $router.options.routes"
-        :key="idx"
-        :to="route.path"
-      >
-        {{ route.name }}
-      </RouterLink>
-    </nav>
-
-    <main>
-      <RouterView />
-    </main>
-  </div>
+    <div>
+        <nav>
+            <RouterLink
+                v-for="(route, idx) in $router.options.routes"
+                :key="idx"
+                :to="route.path"
+            >
+                {{ route.name }}
+            </RouterLink>
+            <LangSwitcher />
+        </nav>
+        <main>
+            <RouterView />
+        </main>
+    </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import LangSwitcher from './components/LangSwitcher.vue'
+</script>
 
 <style></style>
---
+++ src/components/LangSwitcher.vue
@@ -0,0 +1,24 @@
+<template>
+    <slot
+        v-bind="{ locales: $i18n.availableLocales, localeValue: $i18n.locale }"
+    >
+        <select v-model="$i18n.locale">
+            <option v-for="lc in $i18n.availableLocales" :value="lc">
+                {{ lc }}
+            </option>
+        </select>
+    </slot>
+</template>
+
+<script lang="ts" setup>
+import { watch } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { locale } = useI18n()
+watch(locale, (locale) => {
+    document.documentElement.lang = locale
+    localStorage.setItem('locale', locale)
+})
+</script>
+
+<style></style>
