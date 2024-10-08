567165091747d4ab802869151da14080759632ef
Moonmangit <81956440+moonmangit@users.noreply.github.com>
2024-10-08 10:06:37 +0700

    add : linters

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,7 +1,34 @@
 <template>
-  <div>home page</div>
+    <div>
+        home page
+        {{ count }}
+        <button
+            class="p-2 px-3 bg-red-500 text-white"
+            type="button"
+            @click.prevent="inc"
+        >
+            +
+        </button>
+        <button
+            class="p-2 px-3 bg-red-500 text-white"
+            type="button"
+            @click.prevent="dec"
+        >
+            -
+        </button>
+    </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const count = ref(1)
+function inc() {
+    count.value++
+}
+function dec() {
+    count.value--
+}
+</script>
 
 <style></style>
