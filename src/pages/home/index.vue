72f44591891991b6d791a66269da392df53002c8
Moonmangit <81956440+moonmangit@users.noreply.github.com>
2024-10-08 10:33:28 +0700

    add : font

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,34 +1,7 @@
 <template>
-    <div>
-        home page
-        {{ count }}
-        <button
-            class="p-2 px-3 bg-red-500 text-white"
-            type="button"
-            @click.prevent="inc"
-        >
-            +
-        </button>
-        <button
-            class="p-2 px-3 bg-red-500 text-white"
-            type="button"
-            @click.prevent="dec"
-        >
-            -
-        </button>
-    </div>
+    <div>home page {{ $t('home.welcome') }}</div>
 </template>
 
-<script lang="ts" setup>
-import { ref } from 'vue'
-
-const count = ref(1)
-function inc() {
-    count.value++
-}
-function dec() {
-    count.value--
-}
-</script>
+<script lang="ts" setup></script>
 
 <style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  darkMode: "class",
-  plugins: [],
-};
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Prompt', 'sans-serif']
+            }
+        }
+    },
+    darkMode: 'class',
+    plugins: []
+}
